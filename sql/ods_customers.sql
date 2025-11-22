c5ba33bfe9b8f847b6c22a7d44c4e219174963ab
Bowie <84280208+littlebayymax@users.noreply.github.com>
2025-11-22 20:58:48 +0800

    Create ods_customers.sql

---
+++ sql/ods_customers.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS ods_customers (
+  customer_id STRING,
+  name STRING,
+  email STRING,
+  country STRING,
+  signup_date STRING,
+  ingestion_time TIMESTAMP
+)
+PARTITIONED BY (dt STRING)
+LIFECYCLE 30;
+
+INSERT OVERWRITE TABLE ods_customers PARTITION (dt = '${bizdate})
+SELECT
+  customer_id STRING,
+  name STRING,
+  email STRING,
+  country STRING,
+  signup_date STRING,
+  CURRENT_TIMESTAMP()
+FROM oss_customers_raw;
+  
